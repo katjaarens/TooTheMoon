d5f00597e5f705421b4564683017357dc43691f5
katjaarens <257968712+katjaarens@users.noreply.github.com>
2026-09-02 17:40:24 +0200

    --

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,6 +11,11 @@ using System.Diagnostics;
 
 namespace TooTheMoon.Controllers;
 
+public IActionResult Trauzeugen()
+{
+    return View();
+}
+
 public class HomeController : Controller
 {
     private readonly AppDbContext _context;
@@ -175,8 +180,5 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
-    public IActionResult Groomsmaids() // Oder Trauzeugen
-{
-    return View();
-}
+    
 }
