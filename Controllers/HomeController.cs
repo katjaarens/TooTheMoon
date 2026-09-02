bd9781cf6962569083304657acda31f502c97596
katjaarens <257968712+katjaarens@users.noreply.github.com>
2026-09-02 14:55:31 +0200

    neue Seite

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -175,4 +175,8 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+    public IActionResult Groomsmaids() // Oder Trauzeugen
+{
+    return View();
+}
 }
