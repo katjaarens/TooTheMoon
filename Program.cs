bae8b70a6bda9ad1204f78f60ea93d27e8042e4a
katjaarens <257968712+katjaarens@users.noreply.github.com>
2026-09-04 09:02:47 +0200

    blub

--- Program.cs
+++ Program.cs
@@ -61,10 +61,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-    using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<TooTheMoon.Data.AppDbContext>();
-    dbContext.Database.Migrate();
-}
-
 app.Run();
