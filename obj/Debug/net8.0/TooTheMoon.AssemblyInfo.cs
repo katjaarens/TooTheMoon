ef21e032941fa34d57de0e44698a69369308576f
katjaarens <257968712+katjaarens@users.noreply.github.com>
2026-09-08 17:37:56 +0200

    lalala

--- obj/Debug/net8.0/TooTheMoon.AssemblyInfo.cs
+++ obj/Debug/net8.0/TooTheMoon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TooTheMoon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c78165eeb72f9799d46ce0f2340c8657cd6dd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+803f9efe836963f02c1e682220a35c5815f86121")]
 [assembly: System.Reflection.AssemblyProductAttribute("TooTheMoon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TooTheMoon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
