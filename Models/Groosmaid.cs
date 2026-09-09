d157eb5fc0d67d9ca6b9b88341732f80f1d8d606
katjaarens <257968712+katjaarens@users.noreply.github.com>
2026-09-09 07:02:06 +0200

    run1

--- Models/Groosmaid.cs
+++ Models/Groosmaid.cs
@@ -1,15 +1,15 @@
-namespace TooTheMoon.Models;
-
 public class Groomsmaid
 {
-    public int Id { get; set; }
+    public long Id { get; set; }
+
     public string? Name { get; set; }
     public string? Description { get; set; }
     public string? Phone { get; set; }
     public string? ImagePath { get; set; }
-    public string? RoleBadge { get; set; }
-    public string? Anecdote { get; set; }
-    public string? FirstImpression { get; set; }
-    public string? SinceWhen { get; set; }
-    public string? Speciality { get; set; }
+
+    public string? rolebadge { get; set; }
+    public string? anecdote { get; set; }
+    public string? firstimpression { get; set; }
+    public string? sincewhen { get; set; }
+    public string? speciality { get; set; }
 }
